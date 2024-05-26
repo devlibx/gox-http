94f499eef6d43b38ff12aefb6063df74a42c463a
Harish Bohara <harish.bohara@gmail.com>
2024-05-27 00:39:14 +0530

    added tests for new set of gox http helper

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 go install github.com/golang/mock/mockgen@v1.6.0
 
 find . -type f -name '*_mock.go' -exec rm {} \;
+find . -type f -name '*.go-e' -exec rm {} \;
+
 go mod tidy
 go fmt ./...
 go generate ./...
